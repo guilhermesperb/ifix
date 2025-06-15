0290a11a2876cdb2e097a1b9ae35112f049aab19
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-06-15 18:56:16 -0300

    FIX Adjusting create_user_lfs Recipe

--- recipes/create_user_lfs/create_user_lfs.sh
+++ recipes/create_user_lfs/create_user_lfs.sh
@@ -1,27 +1,29 @@
-# Should be used with root user
-if [ "$EUID" -ne 0 ]; then
-  echo "Please, run this script as root"
-  exit 1
-fi
+criar_usuario_lfs()
+{
+  if [ "$EUID" -ne 0 ]; then
+    echo "Por favor, rode como usuário Root"
+    exit 1
+  fi
 
-if [ -z "$LFS" ]; then
-  echo "Variable \$LFS is not defined. Define it with: 'export LFS=/mnt/lfs'"
-  exit 1
-fi
+  if [ -z "$LFS" ]; then
+    echo "Variável \$LFS não está definida. Defina ela com: 'export LFS=/mnt/lfs'"
+    exit 1
+  fi
 
-groupadd lfs
-useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+  groupadd lfs
+  useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 
-echo "Set a password for the 'lfs' user:"
-passwd lfs
+  echo "Cria uma senha para usuário 'lfs': "
+  passwd lfs
 
-chown -v lfs $LFS/{usr{,/*},var,etc,tools}
+  chown -v lfs $LFS/{usr{,/*},var,etc,tools}
 
-if [ "$(uname -m)" == "x86_64" ]; then
-  chown -v lfs $LFS/lib64
-fi
+  if [ "$(uname -m)" == "x86_64" ]; then
+    chown -v lfs $LFS/lib64
+  fi
 
-echo ""
-echo "User 'lfs' create and permissions adjusted"
-echo "Switch to the user with: 'su - lfs'"
-echo ""
+  echo ""
+  echo "Usuário 'lfs' criado e com permissões ajustadas"
+  echo "Mude para este usuário com: 'su - lfs'"
+  echo ""
+}
